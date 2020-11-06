6674b3e6273d071c368d11d5f0f98ae5d529c249
JP <joaoteixeira@eng.ci.ufpb.br>
2020-11-06 17:16:53 -0300

    Update: converter visualization

--- app/main.py
+++ app/main.py
@@ -5,17 +5,23 @@ st.set_option('deprecation.showfileUploaderEncoding', False)
 
 st.title("OpenDIP API - Demonstration Dashboard")
 
-uploaded_file = st.file_uploader("Choose an image...", type= ['png', 'jpg'] )
-if uploaded_file is not None:
-    print(uploaded_file)
-    image = Image.open(uploaded_file)
-    st.image(image, caption='Uploaded Image.', use_column_width=True)
-    
-    converter = Converter()
 
-    yiq_image , yiq_img_arr = converter.RGB_2_YIQ(image_obj = image)
-    st.image(yiq_image, caption='Original RGB image converted to YIQ', use_column_width=True)
 
-    rgb_image , rgb_img_arr = converter.YIQ_2_RGB(yiq_img_arr)
-    st.image(rgb_image, caption='YIQ image converted back to RGB', use_column_width=True)
+
+st.sidebar.title("Configurações")
+select = st.sidebar.selectbox('Escolha a opção que deseja visualizar', ["Conversor RGB-YIQ-RGB", "Construindo", "Construindo", "Construindo"])
+
+if select == "Conversor RGB-YIQ-RGB":
+    uploaded_file = st.file_uploader("Choose an image...", type= ['png', 'jpg'] )
+    if uploaded_file is not None:
+
+        orig_image = Image.open(uploaded_file)
+        converter = Converter()
+
+        converter.visualize_image(orig_image, capt='Uploaded Image')
+
+        yiq_image , yiq_img_arr = converter.RGB_2_YIQ(image_obj = orig_image)
+        rgb_image , rgb_img_arr = converter.YIQ_2_RGB(yiq_img_arr)
+
+        converter.visualize_all_process(orig_image, yiq_image, rgb_image, capt = ['Original RGB Image','YIQ image','Image converted to RGB'])
 
