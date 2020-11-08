647cb16ff48c5a415ef40b570db8f74b84e39f2a
itamar <itamardprf@gmail.com>
2020-11-07 21:13:09 -0300

    correlator v1

--- app/opendip/correlator.py
+++ app/opendip/correlator.py
@@ -11,8 +11,16 @@ class Correlator:
 
     def __padding(self, horizontal_padding, vertical_padding):
         padded_image = np.zeros((self.image.shape[0] + 2 * vertical_padding, self.image.shape[1] + 2 * horizontal_padding, 3))
-        padded_image[vertical_padding : -vertical_padding, horizontal_padding : -horizontal_padding, :] = self.image 
-
+        print("image shape = ",self.image.shape)
+        print("padded image shape = ",padded_image.shape)
+        
+        if vertical_padding == 0:
+            padded_image[:, horizontal_padding : -horizontal_padding, :] = self.image 
+        elif horizontal_padding == 0:
+            padded_image[vertical_padding : -vertical_padding, :, :] = self.image 
+        else:
+            padded_image[vertical_padding : -vertical_padding, horizontal_padding : -horizontal_padding, :] = self.image 
+    
         return padded_image
         
     def apply_correlation(self, image_path, filter_matrix, zero_padding = True, normalization = False):
@@ -21,16 +29,21 @@ class Correlator:
 
         epsilon = 1e-7
         
-        horizontal_padding = self.filter.shape[0]//2
-        vertical_padding = self.filter.shape[1]//2
-
+        vertical_padding = self.filter.shape[0]//2
+        horizontal_padding = self.filter.shape[1]//2
+        
+        if not horizontal_padding and not vertical_padding:
+            print("Could not execute padding due to filter shape. Try a Bi dimensional kernel.")
+            zero_padding = False
+        
         if zero_padding:
             preprocessed_img = self.__padding(horizontal_padding, vertical_padding)
             output = np.zeros((self.image.shape[0], self.image.shape[1], 3))
         else:
             preprocessed_img = self.image
             output = np.zeros((self.image.shape[0] - 2 * vertical_padding, self.image.shape[1] - 2 * horizontal_padding, 3))
-
+    
+        print(output.shape)
         if not normalization:
             for i in range(preprocessed_img.shape[0] - self.filter.shape[0]):
                 for j in range(preprocessed_img.shape[1] - self.filter.shape[1]):
