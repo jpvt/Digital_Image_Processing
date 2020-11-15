47f8cb57837ad02cce9919048875b58932a9ac23
itamar <itamardprf@gmail.com>
2020-11-15 14:56:16 -0300

    gitignored, notebook and correlator fixed

--- app/opendip/correlator.py
+++ app/opendip/correlator.py
@@ -11,7 +11,7 @@ class Correlator:
 
     def __padding(self, horizontal_padding, vertical_padding):
         padded_image = np.zeros((self.image.shape[0] + 2 * vertical_padding, self.image.shape[1] + 2 * horizontal_padding, 3))
-
+        
         if vertical_padding == 0:
             padded_image[:, horizontal_padding : -horizontal_padding, :] = self.image 
         elif horizontal_padding == 0:
@@ -21,45 +21,9 @@ class Correlator:
     
         return padded_image
         
-    def apply_correlation(self, image_path, filter_matrix, zero_padding = False):
-        # self.image = np.array(Image.open(image_path).convert('RGB'))
-        self.image = image_path.copy();
-        self.image = np.array(self.image);
+    def apply_correlation(self, image_path, filter_matrix, zero_padding = True):
+        self.image = np.array(Image.open(image_path).convert('RGB'))
         self.filter = filter_matrix
-  
-        num_rows =np.shape(self.image)[0]
-        num_cols = np.shape(self.image)[1]
-  
-        vertical_padding = self.filter.shape[0]//2
-        horizontal_padding = self.filter.shape[1]//2
-        
-        if not horizontal_padding and not vertical_padding:
-            print("Could not execute padding due to filter shape. Try a Bi dimensional kernel.")
-            zero_padding = False
-        
-        if zero_padding:
-            preprocessed_img = self.__padding(horizontal_padding, vertical_padding)
-            output = np.zeros((num_rows, num_cols, 3))
-        else:
-            preprocessed_img = self.image
-            output = np.zeros((num_rows - 2 * vertical_padding, num_cols - 2 * horizontal_padding, 3))
-    
-        for i in range(preprocessed_img.shape[0] - self.filter.shape[0]):
-            for j in range(preprocessed_img.shape[1] - self.filter.shape[1]):
-                for k in range(3):
-                    output[i,j,k] = np.sum(np.multiply(self.filter, preprocessed_img[i: i + self.filter.shape[0], j: j + self.filter.shape[1], k]))
-        
-        output[output < 0] = 0
-        output[output > 255] = 255
-        
-        return  output
-    
-    def apply_norm_correlation(self, image_path, filter_matrix, zero_padding = True):
-        # self.image = np.array(Image.open(image_path).convert('RGB'))
-        self.image = image_path.copy()
-        self.filter = filter_matrix
-
-        epsilon = 1e-7
         
         vertical_padding = self.filter.shape[0]//2
         horizontal_padding = self.filter.shape[1]//2
@@ -75,10 +39,34 @@ class Correlator:
             preprocessed_img = self.image
             output = np.zeros((self.image.shape[0] - 2 * vertical_padding, self.image.shape[1] - 2 * horizontal_padding, 3))
     
+        for i in range(preprocessed_img.shape[0] - self.filter.shape[0] + 1):
+            for j in range(preprocessed_img.shape[1] - self.filter.shape[1] + 1):
+                for k in range(3):
+                    output[i,j,k] = np.sum(np.multiply(self.filter, preprocessed_img[i: i + self.filter.shape[0], j: j + self.filter.shape[1], k]))
+        
+        output[output < 0] = 0
+        output[output > 255] = 255
+        
+        return self.image, preprocessed_img, output
+    
+    def apply_norm_correlation(self, image_path, filter_matrix):
+        self.image = np.array(Image.open(image_path).convert('RGB'))
+        self.filter = filter_matrix
 
-        filter_diff = self.filter - np.mean(self.filter)
-        print(filter_diff.shape)
-        normalized_filter = filter_diff/np.sum(np.abs(filter_diff + epsilon))
+        epsilon = 1e-7
+        
+        vertical_remnant = self.filter.shape[0]//2
+        horizontal_remnant = self.filter.shape[1]//2
+        
+        preprocessed_img = self.image
+        output = np.zeros((self.image.shape[0] - 2 * vertical_remnant, self.image.shape[1] - 2 * horizontal_remnant))
+    
+
+        filter_diff = []
+        normalized_filter = []
+        for i in range(3):
+            filter_diff.append(self.filter[:,:,i] - np.mean(self.filter[:,:,i]))
+            normalized_filter.append(filter_diff[i]/np.sum(np.abs(filter_diff[i] + epsilon)))
         
         if np.argmax(normalized_filter) == 0:
             print("Warning : Filter has mean zero. Normalization will result in a black image.")
@@ -88,13 +76,14 @@ class Correlator:
                 for k in range(3):
                     window = preprocessed_img[i: i + self.filter.shape[0], j: j + self.filter.shape[1], k]
                     window_diff = window - np.mean(window)
-                    output[i,j,k] += np.sum(np.multiply((window_diff)/np.sum(np.abs(window_diff + epsilon)), normalized_filter))
-                
+                    output[i,j] += np.sum(np.multiply((window_diff)/np.sum(np.abs(window_diff + epsilon)), normalized_filter[k]))
+
+        output /= 3                
         output = (output - np.min(output))/(np.max(output) - np.min(output))
         
-        return self.image, preprocessed_img, output
+        return self.image, output
 
-    def apply_sobel_filter(self, image_path, zero_padding=False, mode="vertical"):
+    def apply_sobel_filter(self, image_path, zero_padding=True, mode="vertical"):
         if mode == "vertical":
             sobel_filter = np.array([[-1,0,1],
                         [-2,0,2],
