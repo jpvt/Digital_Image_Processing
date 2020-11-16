ef16ff114110d75d7a5040c0e852c05b0b2c93e0
itamar <itamardprf@gmail.com>
2020-11-15 21:42:11 -0300

    merged sucess

--- app/opendip/correlator.py
+++ app/opendip/correlator.py
@@ -9,7 +9,7 @@ class Correlator:
         self.image = None
         self.filter = None
 
-    def __padding(self, horizontal_padding, vertical_padding):
+    def padding(self, horizontal_padding, vertical_padding):
         padded_image = np.zeros((self.image.shape[0] + 2 * vertical_padding, self.image.shape[1] + 2 * horizontal_padding, 3))
         
         if vertical_padding == 0:
@@ -33,14 +33,14 @@ class Correlator:
             zero_padding = False
         
         if zero_padding:
-            preprocessed_img = self.__padding(horizontal_padding, vertical_padding)
+            preprocessed_img = self.padding(horizontal_padding, vertical_padding)
             output = np.zeros((self.image.shape[0], self.image.shape[1], 3))
         else:
             preprocessed_img = self.image
             output = np.zeros((self.image.shape[0] - 2 * vertical_padding, self.image.shape[1] - 2 * horizontal_padding, 3))
     
-        for i in range(preprocessed_img.shape[0] - self.filter.shape[0] + 1):
-            for j in range(preprocessed_img.shape[1] - self.filter.shape[1] + 1):
+        for i in range(preprocessed_img.shape[0] - self.filter.shape[0]):
+            for j in range(preprocessed_img.shape[1] - self.filter.shape[1]):
                 for k in range(3):
                     output[i,j,k] = np.sum(np.multiply(self.filter, preprocessed_img[i: i + self.filter.shape[0], j: j + self.filter.shape[1], k]))
         
@@ -82,46 +82,3 @@ class Correlator:
         output = (output - np.min(output))/(np.max(output) - np.min(output))
         
         return self.image, output
-
-    def apply_sobel_filter(self, image_path, zero_padding=True, mode="vertical"):
-        if mode == "vertical":
-            sobel_filter = np.array([[-1,0,1],
-                        [-2,0,2],
-                        [-1,0,1]])
-        elif mode == "horizontal":
-            sobel_filter = np.array([[-1,0,1],
-                        [-2,0,2],
-                        [-1,0,1]]).T
-        else:
-            print("Choose either vertical or Horizontal")
-            return -1
-        
-        return self.apply_correlation(image_path, sobel_filter, zero_padding=zero_padding)
-    
-    def apply_box_filter(self, image_path, box_shape=(3,3), zero_padding=True):
-        divisor = box_shape[0] * box_shape[1]
-        return self.apply_correlation(image_path, np.ones((box_shape[0],box_shape[1]))/divisor, zero_padding)
-    
-    def apply_median_filter(self, image_path, filter_shape=(3,3), zero_padding=True):
-        self.image = np.array(Image.open(image_path).convert('RGB'))
-
-        vertical_padding = filter_shape[0]//2
-        horizontal_padding = filter_shape[1]//2
-
-        if not horizontal_padding and not vertical_padding:
-            print("Could not execute padding due to filter shape. Try a Bi dimensional kernel.")
-            zero_padding = False
-
-        if zero_padding:
-            preprocessed_img = self.__padding(horizontal_padding, vertical_padding)
-            output = np.zeros((self.image.shape[0], self.image.shape[1], 3))
-        else:
-            preprocessed_img = self.image
-            output = np.zeros((self.image.shape[0] - 2 * vertical_padding, self.image.shape[1] - 2 * horizontal_padding, 3))
-
-        for i in range(preprocessed_img.shape[0] - filter_shape[0]):
-            for j in range(preprocessed_img.shape[1] - filter_shape[1]):
-                for k in range(3):
-                    output[i,j,k] = np.median(preprocessed_img[i: i + filter_shape[0], j: j + filter_shape[1], k])
-        
-        return self.image, preprocessed_img, output
--- app/opendip/filter.py
+++ app/opendip/filter.py
@@ -3,6 +3,7 @@ import numpy as np
 import streamlit as st
 from PIL import ImageFilter
 from .converter import Converter
+from .correlator import Correlator
 
 class Filter:
     def __init__(self):
@@ -41,8 +42,63 @@ class Filter:
 
         rgb = rgb_img.copy()
         return rgb
-        
+
+
     def visualize_image(self, image, capt = 'Image'):
         
       st.image(image, caption=capt, use_column_width=True)
 
+    def apply_sobel_filter(self, image_path, zero_padding=True, mode="vertical"):
+
+        c = Correlator()
+
+        if mode == "vertical":
+            sobel_filter = np.array([[-1,0,1],
+                        [-2,0,2],
+                        [-1,0,1]])
+        elif mode == "horizontal":
+            sobel_filter = np.array([[-1,0,1],
+                        [-2,0,2],
+                        [-1,0,1]]).T
+        else:
+            print("Choose either vertical or Horizontal")
+            return -1
+        
+        return c.apply_correlation(image_path, sobel_filter, zero_padding=zero_padding)
+    
+    def apply_box_filter(self, image_path, box_shape=(3,3), zero_padding=True):
+
+        c = Correlator()
+
+        divisor = box_shape[0] * box_shape[1]
+        
+        return c.apply_correlation(image_path, np.ones((box_shape[0],box_shape[1]))/divisor, zero_padding)
+    
+    def apply_median_filter(self, image_path, filter_shape=(3,3), zero_padding=True):
+
+        c = Correlator()
+
+        self.image = np.array(Image.open(image_path).convert('RGB'))
+
+        c.image = self.image
+
+        vertical_padding = filter_shape[0]//2
+        horizontal_padding = filter_shape[1]//2
+
+        if not horizontal_padding and not vertical_padding:
+            print("Could not execute padding due to filter shape. Try a Bi dimensional kernel.")
+            zero_padding = False
+
+        if zero_padding:
+            preprocessed_img = c.padding(horizontal_padding, vertical_padding)
+            output = np.zeros((self.image.shape[0], self.image.shape[1], 3))
+        else:
+            preprocessed_img = self.image
+            output = np.zeros((self.image.shape[0] - 2 * vertical_padding, self.image.shape[1] - 2 * horizontal_padding, 3))
+
+        for i in range(preprocessed_img.shape[0] - filter_shape[0]):
+            for j in range(preprocessed_img.shape[1] - filter_shape[1]):
+                for k in range(3):
+                    output[i,j,k] = np.median(preprocessed_img[i: i + filter_shape[0], j: j + filter_shape[1], k])
+        
+        return self.image, preprocessed_img, output
